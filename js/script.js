a864832553ebcc0b8159da3658c1ea99ad602a61
ilhamaf53 <ilham.ahmadfahrizal@gmail.com>
2020-08-13 01:03:51 +0700

    menambahkan efek parallax

--- js/script.js
+++ js/script.js
@@ -13,3 +13,36 @@ $('.page-scroll').on('click', function (e) {
 
     e.preventDefault();
 });
+
+// parallax
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    // jumbtron
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
+    });
+
+    // portfolio
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .thumbnail').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i + 1));
+        });
+    }
+});
+
+// about
+$(window).on('load', function () {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
